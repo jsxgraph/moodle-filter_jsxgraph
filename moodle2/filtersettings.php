8749ff11279582ae0763f99d7c96db9c90f6ab9b
Andreas <s1aawalt@uni-bayreuth.de>
2018-01-01 20:34:42 +0100

    finishing new version 2018

--- moodle2/filtersettings.php
+++ moodle2/filtersettings.php
@@ -3,13 +3,36 @@
      * Version details
      *
      * @package    jsxgraph filter
-     * @copyright  2017 Michael Gerhaeuser, Matthias Ehmann, Carsten Miller, Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>, Andreas Walter
+     * @copyright  2018 Michael Gerhaeuser, Matthias Ehmann, Carsten Miller, Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>, Andreas Walter
      * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
     
     defined('MOODLE_INTERNAL') || die();
     
+    // Add the placeholder to the description of a setting that should be separated from the following setting
+    $placeholder = '<div class="placeholder" style="height: 65px;padding-top: 10px;padding-bottom: 1rem;margin-left: -33%;"><hr></div>';
+    $last = '<div class="placeholder" style="height: 50px;"></div>';
+    
     if ($ADMIN->fulltree) {
+        $settings->add(new admin_setting_configcheckbox('filter_jsxgraph_jsxfromserver',
+                                                        get_string('jsxfromserver', 'filter_jsxgraph'),
+                                                        get_string('jsxfromserver_desc', 'filter_jsxgraph'),
+                                                        '0'));
+        
+        $settings->add(new admin_setting_configtext_with_maxlength('filter_jsxgraph_serverversion',
+                                                                   get_string('serverversion', 'filter_jsxgraph'),
+                                                                   get_string('serverversion_desc', 'filter_jsxgraph') . $placeholder,
+                                                                   filter_jsxgraph::$recommended_version, PARAM_TEXT, 6, 6));
+        
+        $settings->add(new admin_setting_configselect('filter_jsxgraph_HTMLentities',
+                                                      get_string('HTMLentities', 'filter_jsxgraph'),
+                                                      get_string('HTMLentities_desc', 'filter_jsxgraph') . $placeholder,
+                                                      '1', [get_string('no', 'filter_jsxgraph'), get_string('yes', 'filter_jsxgraph')]));
+        
+        $settings->add(new admin_setting_configtextarea('filter_jsxgraph_globalJS',
+                                                        get_string('globalJS', 'filter_jsxgraph'),
+                                                        get_string('globalJS_desc', 'filter_jsxgraph') . $placeholder,
+                                                        '', PARAM_RAW, 60, 20));
         
         $settings->add(new admin_setting_configtext('filter_jsxgraph_divid',
                                                     get_string('divid', 'filter_jsxgraph'),
@@ -18,7 +41,7 @@
         
         $settings->add(new admin_setting_configtext('filter_jsxgraph_boardvar',
                                                     get_string('boardvar', 'filter_jsxgraph'),
-                                                    get_string('boardvar_desc', 'filter_jsxgraph'),
+                                                    get_string('boardvar_desc', 'filter_jsxgraph') . $placeholder,
                                                     'board'));
         
         $settings->add(new admin_setting_configtext('filter_jsxgraph_width',
@@ -28,12 +51,6 @@
         
         $settings->add(new admin_setting_configtext('filter_jsxgraph_height',
                                                     get_string('height', 'filter_jsxgraph'),
-                                                    get_string('height_desc', 'filter_jsxgraph'),
+                                                    get_string('height_desc', 'filter_jsxgraph') . $last,
                                                     '400', PARAM_INT));
-        
-        $settings->add(new admin_setting_configtext('filter_jsxgraph_HTMLentities',
-                                                    get_string('HTMLentities', 'filter_jsxgraph'),
-                                                    get_string('HTMLentities_desc', 'filter_jsxgraph'),
-                                                    '1', PARAM_BOOL));
-        
-}
+    }
--- moodle2/lang/en/filter_jsxgraph.php
+++ moodle2/lang/en/filter_jsxgraph.php
@@ -3,24 +3,40 @@
      * Version details
      *
      * @package    jsxgraph filter
-     * @copyright  2017 Michael Gerhaeuser, Matthias Ehmann, Carsten Miller, Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>, Andreas Walter
+     * @copyright  2018 Michael Gerhaeuser, Matthias Ehmann, Carsten Miller, Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>, Andreas Walter
      * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
     
     $string['filtername'] = 'JSXGraph';
     
-    $string['divid'] = 'div id';
-    $string['divid_desc'] = 'ID of the division containing JSXGraph. Default: box. Number is added automatically, e.g. box0, box1, ...';
+    $string['yes'] = 'yes';
+    $string['no'] = 'no';
     
-    $string['boardvar'] = 'board var name';
-    $string['boardvar_desc'] = 'Variable name of the JSXGraph board (needed only if file is read)';
+    $string['error'] = 'ERROR:';
+    $string['errorNotFound_pre'] = 'There is no JSX version ';
+    $string['errorNotFound_post'] = ' on CDN. The JSXGraph core could not be loaded. Please contact your admin.';
     
-    $string['width'] = 'width';
-    $string['width_desc'] = 'Width of JSXGraph container';
+    $string['jsxfromserver'] = 'JSXGraph from server';
+    $string['jsxfromserver_desc'] = 'Select whether the plugin is using the server version of JSXGraph core, or the locally provided one supplied with the plugin. <b>Attention:</b> there must be entered a valid version number in "<a href="#admin-filter_jsxgraph_serverversion">server version</a>"!';
     
-    $string['height'] = 'height';
-    $string['height_desc'] = 'Height of JSXGraph container';
+    $string['serverversion'] = 'server version';
+    $string['serverversion_desc'] = 'If "<a href="#admin-filter_jsxgraph_jsxfromserver">JSXGraph from server</a>" is checked, the version entered here is loaded by the server. Look at <a href="http://jsxgraph.uni-bayreuth.de/wp/previousreleases/" target="_blank">http://jsxgraph.uni-bayreuth.de/wp/previousreleases/</a> to see, which version is loaded from CDN. Type only the version number.';
     
     $string['HTMLentities'] = 'HTMLentities';
-    $string['HTMLentities_desc'] = 'Decide wether HTMLentities like "&", "<",... are supported within the JavaScript code for JSXGraph. Type 1 for "true", 0 for "false".';
-
+    $string['HTMLentities_desc'] = 'Decide wether HTMLentities like "&", "<",... are supported within the JavaScript code for JSXGraph.';
+    
+    $string['globalJS'] = 'Global JavaScript';
+    $string['globalJS_desc'] = 'Define a general JavaScript code that is loaded in each JSXGraph tag before the code contained in it. To type special characters like "<" use HTMLentities.';
+    
+    $string['divid'] = 'div id';
+    $string['divid_desc'] = 'ID of the division containing JSXGraph. Number is added automatically, e.g. box0, box1,...';
+    
+    $string['boardvar'] = 'board var name';
+    $string['boardvar_desc'] = 'Variable name of the JSXGraph board (needed only if file is read).';
+    
+    $string['width'] = 'width';
+    $string['width_desc'] = 'Width of JSXGraph container.';
+    
+    $string['height'] = 'height';
+    $string['height_desc'] = 'Height of JSXGraph container.';
+    
