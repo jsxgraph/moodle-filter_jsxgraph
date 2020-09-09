1c63e555c295430466554781d0515d674dc6e462
Andreas Walter <andreas.walter@uni-bayreuth.de>
2020-09-09 09:30:16 +0200

    prepare release v1.1.0-new

--- version.php
+++ version.php
@@ -34,8 +34,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020090201;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 20200900900;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015051100;         // Requires this Moodle version.
 $plugin->component = 'filter_jsxgraph'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.1.0';            // JSXGraph version.
+$plugin->release = 'v1.1.0-new';            // JSXGraph version.
