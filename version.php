0347cef736d61ef900ab0b34a4b814a3ad616a7c
Andreas Walter <andreas.walter@uni-bayreuth.de>
2020-09-10 15:22:18 +0200

    change version.php

--- version.php
+++ version.php
@@ -36,6 +36,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2020091000;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015051100;         // Requires this Moodle version.
+$plugin->supported = [29, 39];          // Moodle versions
 $plugin->component = 'filter_jsxgraph'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.1.0-new';            // JSXGraph version.
