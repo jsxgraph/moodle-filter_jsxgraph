2222bea8ee3be5d038e8c5b6c8e35b98cde7dc7d
Andreas Walter <andreas.walter@uni-bayreuth.de>
2020-09-10 15:44:31 +0200

    release v2020091001

--- version.php
+++ version.php
@@ -34,7 +34,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020091000;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2020091001;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015051100;         // Requires this Moodle version.
 $plugin->supported = [29, 39];          // Moodle versions
 $plugin->component = 'filter_jsxgraph'; // Full name of the plugin (used for diagnostics).
